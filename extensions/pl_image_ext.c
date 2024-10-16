69699a41d1db151e6dd9400b3b995566cc7c5667
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2024-10-15 19:28:29 -0500

    feat: 1.0 image extension

--- extensions/pl_image_ext.c
+++ extensions/pl_image_ext.c
@@ -15,6 +15,7 @@ Index of this file:
 //-----------------------------------------------------------------------------
 
 #include "pl.h"
+#include "pl_string.h"
 #include "pl_image_ext.h"
 #include "stb_image.h"
 #include "stb_image_write.h"
@@ -24,45 +25,52 @@ Index of this file:
 //-----------------------------------------------------------------------------
 
 static bool
-pl__is_hdr(const char* pcFileName)
+pl__get_info(char const* pcFilename, plImageInfo* ptInfoOut)
 {
-    return stbi_is_hdr(pcFileName);
+    ptInfoOut->b16Bit = stbi_is_16_bit(pcFilename);
+    ptInfoOut->bHDR = stbi_is_hdr(pcFilename);
+
+    return stbi_info(pcFilename, &ptInfoOut->iWidth, &ptInfoOut->iHeight, &ptInfoOut->iChannels);
 }
 
 static bool
-pl__is_hdr_from_memory(const unsigned char* pcBuffer, int iLength)
+pl__get_info_from_memory(const unsigned char* pcBuffer, int iLength, plImageInfo* ptInfoOut)
 {
-    return stbi_is_hdr_from_memory(pcBuffer, iLength);
+    ptInfoOut->b16Bit = stbi_is_16_bit_from_memory(pcBuffer, iLength);
+    ptInfoOut->bHDR = stbi_is_hdr_from_memory(pcBuffer, iLength);
+
+    return stbi_info_from_memory(pcBuffer, iLength, &ptInfoOut->iWidth, &ptInfoOut->iHeight, &ptInfoOut->iChannels);
 }
 
 static bool
-pl__write_png(char const *pcFileName, int iW, int iH, int iComp, const void *pData, int iByteStride)
+pl_write_image(char const *pcFileName, const void *pData, const plImageWriteInfo* ptInfo)
 {
-    return stbi_write_png(pcFileName, iW, iH, iComp, pData, iByteStride);
-}
+    const char* pcExt = pl_str_get_file_extension(pcFileName, NULL, 0);
+    
+    if(pcExt == NULL)
+        return false;
 
-static bool
-pl__write_bmp(char const *pcFileName, int iW, int iH, int iComp, const void *pData)
-{
-    return stbi_write_bmp(pcFileName, iW, iH, iComp, pData);
-}
+    // png
+    if(pcExt[0] == 'p' || pcExt[0] == 'P')
+        return stbi_write_png(pcFileName, ptInfo->iWidth, ptInfo->iHeight, ptInfo->iComponents, pData, ptInfo->iByteStride);
+    
+    // jpg
+    else if(pcExt[0] == 'j' || pcExt[0] == 'J')
+        return stbi_write_jpg(pcFileName, ptInfo->iWidth, ptInfo->iHeight, ptInfo->iComponents, pData, ptInfo->iQuality);
 
-static bool
-pl__write_tga(char const *pcFileName, int iW, int iH, int iComp, const void *pData)
-{
-    return stbi_write_tga(pcFileName, iW, iH, iComp, pData);
-}
+    // bmp
+    else if(pcExt[0] == 'b' || pcExt[0] == 'B')
+        return stbi_write_bmp(pcFileName, ptInfo->iWidth, ptInfo->iHeight, ptInfo->iComponents, pData);
 
-static bool
-pl__write_jpg(char const *pcFileName, int iW, int iH, int iComp, const void *pData, int iQuality)
-{
-    return stbi_write_jpg(pcFileName, iW, iH, iComp, pData, iQuality);
-}
+    // hdr
+    else if(pcExt[0] == 'h' || pcExt[0] == 'H')
+        return stbi_write_hdr(pcFileName, ptInfo->iWidth, ptInfo->iHeight, ptInfo->iComponents, (const float*)pData);
 
-static bool
-pl__write_hdr(char const *pcFileName, int iW, int iH, int iComp, const float *pfData)
-{
-    return stbi_write_hdr(pcFileName, iW, iH, iComp, pfData);
+    // tga
+    else if(pcExt[0] == 't' || pcExt[0] == 'T')
+        return stbi_write_tga(pcFileName, ptInfo->iWidth, ptInfo->iHeight, ptInfo->iComponents, pData);
+
+    return false;
 }
 
 //-----------------------------------------------------------------------------
@@ -73,22 +81,20 @@ static const plImageI*
 pl_load_image_api(void)
 {
     static const plImageI tApi = {
-        .load_from_memory     = stbi_load_from_memory,
-        .load_hdr_from_memory = stbi_loadf_from_memory,
-        .load                 = stbi_load,
-        .load_hdr             = stbi_loadf,
-        .free                 = stbi_image_free,
-        .write_png            = pl__write_png,
-        .write_bmp            = pl__write_bmp,
-        .write_tga            = pl__write_tga,
-        .write_jpg            = pl__write_jpg,
-        .write_hdr            = pl__write_hdr,
-        .is_hdr               = pl__is_hdr,
-        .is_hdr_from_memory   = pl__is_hdr_from_memory,
-        .hdr_to_ldr_gamma     = stbi_hdr_to_ldr_gamma,
-        .hdr_to_ldr_scale     = stbi_hdr_to_ldr_scale,
-        .ldr_to_hdr_gamma     = stbi_ldr_to_hdr_gamma,
-        .ldr_to_hdr_scale     = stbi_ldr_to_hdr_scale
+        .get_info               = pl__get_info,
+        .get_info_from_memory   = pl__get_info_from_memory,
+        .load_from_memory       = stbi_load_from_memory,
+        .load_16bit_from_memory = stbi_load_16_from_memory,
+        .load_hdr_from_memory   = stbi_loadf_from_memory,
+        .load                   = stbi_load,
+        .load_16bit             = stbi_load_16,
+        .load_hdr               = stbi_loadf,
+        .free                   = stbi_image_free,
+        .write                  = pl_write_image,
+        .set_hdr_to_ldr_gamma   = stbi_hdr_to_ldr_gamma,
+        .set_hdr_to_ldr_scale   = stbi_hdr_to_ldr_scale,
+        .set_ldr_to_hdr_gamma   = stbi_ldr_to_hdr_gamma,
+        .set_ldr_to_hdr_scale   = stbi_ldr_to_hdr_scale
     };
     return &tApi;
 }
--- extensions/pl_image_ext.h
+++ extensions/pl_image_ext.h
@@ -7,7 +7,9 @@ Index of this file:
 // [SECTION] header mess
 // [SECTION] includes
 // [SECTION] APIs
-// [SECTION] public api structs
+// [SECTION] forward declarations & basic types
+// [SECTION] public api struct
+// [SECTION] structs
 */
 
 //-----------------------------------------------------------------------------
@@ -16,6 +18,10 @@ Index of this file:
 
 #ifndef PL_IMAGE_EXT_H
 #define PL_IMAGE_EXT_H
+
+// extension version (format XYYZZ)
+#define PL_IMAGE_EXT_VERSION    "1.0.0"
+#define PL_IMAGE_EXT_VERSION_NUM 10000
 
 //-----------------------------------------------------------------------------
 // [SECTION] includes
@@ -31,36 +37,65 @@ Index of this file:
 typedef struct _plImageI plImageI;
 
 //-----------------------------------------------------------------------------
-// [SECTION] public api structs
+// [SECTION] forward declarations & basic types
+//-----------------------------------------------------------------------------
+
+// basic types
+typedef struct _plImageInfo      plImageInfo;
+typedef struct _plImageWriteInfo plImageWriteInfo;
+
+//-----------------------------------------------------------------------------
+// [SECTION] public api struct
 //-----------------------------------------------------------------------------
 
 typedef struct _plImageI
 {
-    // query for which interface to use
-    bool (*is_hdr)            (const char* pcFileName);
-    bool (*is_hdr_from_memory)(const unsigned char* pcBuffer, int iLength);
+    // query image info
+    bool (*get_info)           (char const* pcFilename, plImageInfo* ptInfoOut);
+    bool (*get_info_from_memory)(const unsigned char* pcBuffer, int iLength, plImageInfo* ptInfoOut);
 
     // reading LDR (HDR will be remapped through this interface)
-    unsigned char* (*load_from_memory) (const unsigned char* pcBuffer, int iLength, int* piX, int* piY, int* piChannels, int iDesiredChannels);
-    unsigned char* (*load)             (char const* pcFilename, int* piX, int* piY, int* piChannels, int iDesiredChannels);
-    void           (*hdr_to_ldr_gamma) (float fGamma); // default 2.2f
-    void           (*hdr_to_ldr_scale) (float fScale); // default 1.0f
+    unsigned char*  (*load_from_memory)      (const unsigned char* pcBuffer, int iLength, int* piX, int* piY, int* piChannels, int iDesiredChannels);
+    unsigned char*  (*load)                  (char const* pcFilename, int* piX, int* piY, int* piChannels, int iDesiredChannels);
+    unsigned short* (*load_16bit_from_memory)(const unsigned char* pcBuffer, int iLength, int* piX, int* piY, int* piChannels, int iDesiredChannels);
+    unsigned short* (*load_16bit)            (char const* pcFilename, int* piX, int* piY, int* piChannels, int iDesiredChannels);
+    void            (*set_hdr_to_ldr_gamma)  (float); // default 2.2f
+    void            (*set_hdr_to_ldr_scale)  (float); // default 1.0f
     
     // reading HDR (LDR will be promoted to floating point values)
     float* (*load_hdr_from_memory)(const unsigned char* pcBuffer, int iLength, int* piX, int* piY, int* piChannels, int iDesiredChannels);
     float* (*load_hdr)            (char const* pcFilename, int* piX, int* piY, int* piChannels, int iDesiredChannels);
-    void   (*ldr_to_hdr_gamma)    (float fGamma); // default 2.2f
-    void   (*ldr_to_hdr_scale)    (float fScale); // default 1.0f
+    void   (*set_ldr_to_hdr_gamma)(float); // default 2.2f
+    void   (*set_ldr_to_hdr_scale)(float); // default 1.0f
     
     // call when finished with memory
     void (*free)(void* pRetValueFromLoad);
 
-    // writing to disk
-    bool (*write_png)(char const *pcFileName, int iW, int iH, int iComp, const void *pData, int iByteStride);
-    bool (*write_bmp)(char const *pcFileName, int iW, int iH, int iComp, const void *pData);
-    bool (*write_tga)(char const *pcFileName, int iW, int iH, int iComp, const void *pData);
-    bool (*write_jpg)(char const *pcFileName, int iW, int iH, int iComp, const void *pData, int iQuality);
-    bool (*write_hdr)(char const *pcFileName, int iW, int iH, int iComp, const float *pfData);
+    // writing to disk (currently supports png, jpg, bmp, hdr, tga)
+    bool (*write)(char const *pcFileName, const void *pData, const plImageWriteInfo*);
 } plImageI;
+
+//-----------------------------------------------------------------------------
+// [SECTION] structs
+//-----------------------------------------------------------------------------
+
+typedef struct _plImageInfo
+{
+    int iWidth;
+    int iHeight;
+    int iChannels;
+    bool b16Bit;
+    bool bHDR;
+} plImageInfo;
+
+typedef struct _plImageWriteInfo
+{
+    int iWidth;
+    int iHeight;
+    int iComponents;
+
+    int iQuality;    // jpeg only
+    int iByteStride; // png only
+} plImageWriteInfo;
 
 #endif // PL_IMAGE_EXT_H
