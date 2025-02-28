90d906eaa0a0661d99ee5482fcdebcb0d59ee7d1
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2025-02-27 21:48:09 -0600

    refac: change camera script to reflect unreal camera controls

--- extensions/pl_script_camera.c
+++ extensions/pl_script_camera.c
@@ -14,12 +14,16 @@ Index of this file:
 // [SECTION] includes
 //-----------------------------------------------------------------------------
 
+#include <math.h>
 #include "pl.h"
 #include "pl_script_ext.h"
+#define PL_MATH_INCLUDE_FUNCTIONS
+#include "pl_math.h"
 
 // extensions
 #include "pl_ecs_ext.h"
 #include "pl_ui_ext.h"
+#include "pl_gizmo_ext.h"
 
 //-----------------------------------------------------------------------------
 // [SECTION] global data & APIs
@@ -30,20 +34,28 @@ const plIOI*     gptIO     = NULL;
 const plCameraI* gptCamera = NULL;
 const plEcsI*    gptEcs    = NULL;
 const plUiI*     gptUi     = NULL;
+const plGizmoI*  gptGizmo  = NULL;
+
+static float gfOriginalFOV = 0.0f;
 
 //-----------------------------------------------------------------------------
 // [SECTION] implementation
 //-----------------------------------------------------------------------------
 
-// static void
-// pl_script_run(plComponentLibrary* ptLibrary, plEntity tEntity)
-// {
-
-// }
+static void
+pl_script_setup(plComponentLibrary* ptLibrary, plEntity tEntity)
+{
+    plCameraComponent* ptCamera = gptEcs->get_component(ptLibrary, PL_COMPONENT_TYPE_CAMERA, tEntity);
+    gfOriginalFOV = ptCamera->fFieldOfView;
+}
 
 static void
 pl_script_run(plComponentLibrary* ptLibrary, plEntity tEntity)
 {
+
+    if(gptGizmo->active())
+        return;
+
     plCameraComponent* ptCamera = gptEcs->get_component(ptLibrary, PL_COMPONENT_TYPE_CAMERA, tEntity);
 
     plIO* ptIO = gptIO->get_io();
@@ -51,25 +63,74 @@ pl_script_run(plComponentLibrary* ptLibrary, plEntity tEntity)
     static const float fCameraTravelSpeed = 4.0f;
     static const float fCameraRotationSpeed = 0.005f;
 
-    // camera space
     bool bOwnKeyboard = gptUi->wants_keyboard_capture();
-    if(!bOwnKeyboard)
-    {
-        if(gptIO->is_key_down(PL_KEY_W)) gptCamera->translate(ptCamera,  0.0f,  0.0f,  fCameraTravelSpeed * ptIO->fDeltaTime);
-        if(gptIO->is_key_down(PL_KEY_S)) gptCamera->translate(ptCamera,  0.0f,  0.0f, -fCameraTravelSpeed* ptIO->fDeltaTime);
-        if(gptIO->is_key_down(PL_KEY_A)) gptCamera->translate(ptCamera, -fCameraTravelSpeed * ptIO->fDeltaTime,  0.0f,  0.0f);
-        if(gptIO->is_key_down(PL_KEY_D)) gptCamera->translate(ptCamera,  fCameraTravelSpeed * ptIO->fDeltaTime,  0.0f,  0.0f);
+    bool bOwnMouse = gptUi->wants_mouse_capture();
 
-        // world space
-        if(gptIO->is_key_down(PL_KEY_F)) { gptCamera->translate(ptCamera,  0.0f, -fCameraTravelSpeed * ptIO->fDeltaTime,  0.0f); }
-        if(gptIO->is_key_down(PL_KEY_R)) { gptCamera->translate(ptCamera,  0.0f,  fCameraTravelSpeed * ptIO->fDeltaTime,  0.0f); }
-    }
-
-    if(gptIO->is_mouse_dragging(PL_MOUSE_BUTTON_MIDDLE, 1.0f))
+    if(!bOwnKeyboard && !bOwnMouse)
     {
-        const plVec2 tMouseDelta = gptIO->get_mouse_drag_delta(PL_MOUSE_BUTTON_MIDDLE, 1.0f);
-        gptCamera->rotate(ptCamera,  -tMouseDelta.y * fCameraRotationSpeed,  -tMouseDelta.x * fCameraRotationSpeed);
-        gptIO->reset_mouse_drag_delta(PL_MOUSE_BUTTON_MIDDLE);
+
+        bool bRMB = gptIO->is_mouse_down(PL_MOUSE_BUTTON_RIGHT);
+        bool bLMB = gptIO->is_mouse_down(PL_MOUSE_BUTTON_LEFT);
+
+        if(gptIO->is_mouse_clicked(PL_MOUSE_BUTTON_RIGHT, false))
+        {
+            gfOriginalFOV = ptCamera->fFieldOfView;
+        }
+        else if(gptIO->is_mouse_released(PL_MOUSE_BUTTON_RIGHT))
+        {
+            ptCamera->fFieldOfView = gfOriginalFOV;
+        }
+
+
+        // camera space
+        
+        if(bRMB)
+        {
+            if(gptIO->is_key_down(PL_KEY_W)) gptCamera->translate(ptCamera,  0.0f,  0.0f,  fCameraTravelSpeed * ptIO->fDeltaTime);
+            if(gptIO->is_key_down(PL_KEY_S)) gptCamera->translate(ptCamera,  0.0f,  0.0f, -fCameraTravelSpeed* ptIO->fDeltaTime);
+            if(gptIO->is_key_down(PL_KEY_A)) gptCamera->translate(ptCamera, -fCameraTravelSpeed * ptIO->fDeltaTime,  0.0f,  0.0f);
+            if(gptIO->is_key_down(PL_KEY_D)) gptCamera->translate(ptCamera,  fCameraTravelSpeed * ptIO->fDeltaTime,  0.0f,  0.0f);
+
+            // world space
+            if(gptIO->is_key_down(PL_KEY_Q)) { gptCamera->translate(ptCamera,  0.0f, -fCameraTravelSpeed * ptIO->fDeltaTime,  0.0f); }
+            if(gptIO->is_key_down(PL_KEY_E)) { gptCamera->translate(ptCamera,  0.0f,  fCameraTravelSpeed * ptIO->fDeltaTime,  0.0f); }
+
+            if(gptIO->is_key_down(PL_KEY_Z))
+            {
+                ptCamera->fFieldOfView += 0.25f * (PL_PI / 180.0f);
+                ptCamera->fFieldOfView = pl_minf(ptCamera->fFieldOfView, 2.96706f);
+            }
+            if(gptIO->is_key_down(PL_KEY_C))
+            {
+                ptCamera->fFieldOfView -= 0.25f * (PL_PI / 180.0f);
+
+                ptCamera->fFieldOfView = pl_maxf(ptCamera->fFieldOfView, 0.03f);
+            }
+        }
+
+        if(bLMB && gptIO->is_mouse_dragging(PL_MOUSE_BUTTON_RIGHT, 1.0f))
+        {
+            const plVec2 tMouseDelta = gptIO->get_mouse_drag_delta(PL_MOUSE_BUTTON_RIGHT, 1.0f);
+            gptCamera->translate(ptCamera,  tMouseDelta.x * fCameraTravelSpeed * ptIO->fDeltaTime, -tMouseDelta.y * fCameraTravelSpeed * ptIO->fDeltaTime, 0.0f);
+            gptIO->reset_mouse_drag_delta(PL_MOUSE_BUTTON_RIGHT);
+            gptIO->reset_mouse_drag_delta(PL_MOUSE_BUTTON_LEFT);
+        }
+
+        else if(gptIO->is_mouse_dragging(PL_MOUSE_BUTTON_RIGHT, 1.0f))
+        {
+            const plVec2 tMouseDelta = gptIO->get_mouse_drag_delta(PL_MOUSE_BUTTON_RIGHT, 1.0f);
+            gptCamera->rotate(ptCamera,  -tMouseDelta.y * fCameraRotationSpeed,  -tMouseDelta.x * fCameraRotationSpeed);
+            gptIO->reset_mouse_drag_delta(PL_MOUSE_BUTTON_RIGHT);
+        }
+
+        else if(bLMB)
+        {
+            const plVec2 tMouseDelta = gptIO->get_mouse_drag_delta(PL_MOUSE_BUTTON_LEFT, 1.0f);
+            gptCamera->rotate(ptCamera,  0.0f,  -tMouseDelta.x * fCameraRotationSpeed);
+            ptCamera->tPos.x += -tMouseDelta.y * fCameraTravelSpeed * ptIO->fDeltaTime * sinf(ptCamera->fYaw);
+            ptCamera->tPos.z += -tMouseDelta.y * fCameraTravelSpeed * ptIO->fDeltaTime * cosf(ptCamera->fYaw);
+            gptIO->reset_mouse_drag_delta(PL_MOUSE_BUTTON_LEFT);
+        }
     }
 
     gptCamera->update(ptCamera);
@@ -87,9 +148,10 @@ pl_load_script(plApiRegistryI* ptApiRegistry, bool bReload)
     gptCamera = pl_get_api_latest(ptApiRegistry, plCameraI);
     gptIO     = pl_get_api_latest(ptApiRegistry, plIOI);
     gptUi     = pl_get_api_latest(ptApiRegistry, plUiI);
+    gptGizmo  = pl_get_api_latest(ptApiRegistry, plGizmoI);
 
     const plScriptI tApi = {
-        .setup = NULL,
+        .setup = pl_script_setup,
         .run   = pl_script_run
     };
 
