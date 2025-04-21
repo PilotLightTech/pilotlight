bbe3c26f787920fea1f928079dbb53c231600637
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2025-04-20 21:09:18 -0500

    chore: bump version to v0.1.2

--- src/pl.h
+++ src/pl.h
@@ -24,9 +24,9 @@ Index of this file:
 #define PL_H
 
 // global version XYYZZ
-#define PILOT_LIGHT_VERSION_STRING "0.1.1"
-#define PILOT_LIGHT_VERSION_NUM     00101
-#define PILOT_LIGHT_VERSION         {0, 1, 1}
+#define PILOT_LIGHT_VERSION_STRING "0.1.2"
+#define PILOT_LIGHT_VERSION_NUM     00102
+#define PILOT_LIGHT_VERSION         {0, 1, 2}
 
 //-----------------------------------------------------------------------------
 // [SECTION] apis
