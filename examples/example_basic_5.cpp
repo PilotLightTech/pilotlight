cc27a081675e5717c8019089d2da094f14c35368
Jonathan Hoffstadt <jonathan.h.hoffstadt@nasa.gov>
2025-06-20 12:38:10 -0500

    WIP

--- examples/example_basic_5.cpp
+++ examples/example_basic_5.cpp
@@ -1,54 +1,3 @@
-/*
-   example_basic_5.cpp
-     - demonstrates loading APIs
-     - demonstrates loading extensions
-     - demonstrates hot reloading
-     - demonstrates minimal use of graphics extension
-     - demonstrates ui extension
-*/
-
-/*
-Index of this file:
-// [SECTION] quick notes
-// [SECTION] includes
-// [SECTION] structs
-// [SECTION] apis
-// [SECTION] pl_app_load
-// [SECTION] pl_app_shutdown
-// [SECTION] pl_app_resize
-// [SECTION] pl_app_update
-// [SECTION] full demo
-// [SECTION] unity build
-*/
-
-//-----------------------------------------------------------------------------
-// [SECTION] quick notes
-//-----------------------------------------------------------------------------
-
-/*
-    This example is isn't really here to demonstrate anything (yet). It
-    utilizes an unstable extension and experimental backend but it can still
-    be used to experiment with Dear ImGui or used as a template to create tool
-    with this version of Pilot Light. For reference, the experimental backend
-    is currently "experimental" because it has some Dear ImGui setup code in
-    it. Once more features are exposed by the windows extension & backend, 
-    the Dear ImGui code can be completely handled by users or extension. Until
-    then, it will be experimental.
-
-    IMPORTANT:
-        To use this example, you must build Pilot Light and the examples with
-        "debug_experimental" or "release_experimental".
-
-        ~~~> build.bat -c debug_release
-        or
-        ~~~> build.sh -c debug_release
-
-    Dear ImGui is an immediate mode UI library similar to our UI extension (which
-    was actually inspired by both Dear ImGui and Nuklear). There are differences
-    so make sure you check the header files (documentation) for both before 
-    assumming the APIs work the exact same way. In particular when it comes to
-    paired calls for things like begin/end widgets/windows/containers.
-*/
 
 //-----------------------------------------------------------------------------
 // [SECTION] includes
@@ -57,18 +6,81 @@ Index of this file:
 #include <stdlib.h> // malloc, free
 #include <stdio.h>  // printf
 #include <string.h> // memset
-#include "pl.h"
 
-// extensions
-#include "pl_ui_ext.h"
-#include "pl_draw_backend_ext.h"
-#include "pl_starter_ext.h"
+// pilot light
+#include "pl.h"
+#include "pl_memory.h"
+#include "pl_string.h"
+#define PL_MATH_INCLUDE_FUNCTIONS
+#include "pl_math.h"
+#include "pl_icons.h"
+#include "pl_json.h"
+
+// stable extensions
+#include "pl_image_ext.h"
+#include "pl_profile_ext.h"
+#include "pl_log_ext.h"
+#include "pl_stats_ext.h"
 #include "pl_graphics_ext.h"
+#include "pl_tools_ext.h"
+#include "pl_job_ext.h"
+#include "pl_draw_ext.h"
+#include "pl_draw_backend_ext.h"
+#include "pl_ui_ext.h"
+#include "pl_shader_ext.h"
+#include "pl_string_intern_ext.h"
+#include "pl_platform_ext.h"
+#include "pl_console_ext.h"
+#include "pl_screen_log_ext.h"
+#include "pl_starter_ext.h"
+#include "pl_pak_ext.h"
+#include "pl_datetime_ext.h"
+#include "pl_config_ext.h"
+#include "pl_vfs_ext.h"
+#include "pl_compress_ext.h"
 
 // dear imgui
 #include "pl_dear_imgui_ext.h"
 #include "imgui.h"
 #include "implot.h"
+
+//-----------------------------------------------------------------------------
+// [SECTION] global apis
+//-----------------------------------------------------------------------------
+
+const plWindowI*       gptWindows       = nullptr;
+const plStatsI*        gptStats         = nullptr;
+const plGraphicsI*     gptGfx           = nullptr;
+const plToolsI*        gptTools         = nullptr;
+const plJobI*          gptJobs          = nullptr;
+const plDrawI*         gptDraw          = nullptr;
+const plDrawBackendI*  gptDrawBackend   = nullptr;
+const plUiI*           gptUI            = nullptr;
+const plIOI*           gptIO            = nullptr;
+const plShaderI*       gptShader        = nullptr;
+const plMemoryI*       gptMemory        = nullptr;
+const plNetworkI*      gptNetwork       = nullptr;
+const plStringInternI* gptString        = nullptr;
+const plProfileI*      gptProfile       = nullptr;
+const plFileI*         gptFile          = nullptr;
+const plConsoleI*      gptConsole       = nullptr;
+const plScreenLogI*    gptScreenLog     = nullptr;
+const plConfigI*       gptConfig        = nullptr;
+const plStarterI*      gptStarter       = nullptr;
+const plVfsI*          gptVfs           = nullptr;
+const plPakI*          gptPak           = nullptr;
+const plDateTimeI*     gptDateTime      = nullptr;
+const plCompressI*     gptCompress      = nullptr;
+const plDearImGuiI*    gptDearImGui     = nullptr;
+
+#define PL_ALLOC(x)      gptMemory->tracked_realloc(nullptr, (x), __FILE__, __LINE__)
+#define PL_REALLOC(x, y) gptMemory->tracked_realloc((x), (y), __FILE__, __LINE__)
+#define PL_FREE(x)       gptMemory->tracked_realloc((x), 0, __FILE__, __LINE__)
+
+#define PL_DS_ALLOC(x)                      gptMemory->tracked_realloc(nullptr, (x), __FILE__, __LINE__)
+#define PL_DS_ALLOC_INDIRECT(x, FILE, LINE) gptMemory->tracked_realloc(nullptr, (x), FILE, LINE)
+#define PL_DS_FREE(x)                       gptMemory->tracked_realloc((x), 0, __FILE__, __LINE__)
+#include "pl_ds.h"
 
 //-----------------------------------------------------------------------------
 // [SECTION] structs
@@ -77,19 +89,11 @@ Index of this file:
 typedef struct _plAppData
 {
     plWindow* ptWindow;
+
+    // UI options
+    bool bShowImGuiDemo;
+    bool bShowImPlotDemo;
 } plAppData;
-
-//-----------------------------------------------------------------------------
-// [SECTION] apis
-//-----------------------------------------------------------------------------
-
-const plIOI*          gptIO          = NULL;
-const plWindowI*      gptWindows     = NULL;
-const plUiI*          gptUi          = NULL;
-const plDrawBackendI* gptDrawBackend = NULL;
-const plStarterI*     gptStarter     = NULL;
-const plDearImGuiI*   gptDearImGui   = NULL;
-const plGraphicsI*    gptGfx         = NULL;
 
 //-----------------------------------------------------------------------------
 // [SECTION] pl_app_load
@@ -128,13 +132,33 @@ pl_app_load(plApiRegistryI* ptApiRegistry, plAppData* ptAppData)
 
         // re-retrieve the apis since we are now in
         // a different dll/so
-        gptIO          = pl_get_api_latest(ptApiRegistry, plIOI);
-        gptWindows     = pl_get_api_latest(ptApiRegistry, plWindowI);
-        gptDrawBackend = pl_get_api_latest(ptApiRegistry, plDrawBackendI);
-        gptUi          = pl_get_api_latest(ptApiRegistry, plUiI);
-        gptStarter     = pl_get_api_latest(ptApiRegistry, plStarterI);
-        gptDearImGui   = pl_get_api_latest(ptApiRegistry, plDearImGuiI);
-        gptGfx         = pl_get_api_latest(ptApiRegistry, plGraphicsI);
+        gptWindows       = pl_get_api_latest(ptApiRegistry, plWindowI);
+        gptStats         = pl_get_api_latest(ptApiRegistry, plStatsI);
+        gptGfx           = pl_get_api_latest(ptApiRegistry, plGraphicsI);
+        gptTools         = pl_get_api_latest(ptApiRegistry, plToolsI);
+        gptJobs          = pl_get_api_latest(ptApiRegistry, plJobI);
+        gptDraw          = pl_get_api_latest(ptApiRegistry, plDrawI);
+        gptDrawBackend   = pl_get_api_latest(ptApiRegistry, plDrawBackendI);
+        gptUI            = pl_get_api_latest(ptApiRegistry, plUiI);
+        gptIO            = pl_get_api_latest(ptApiRegistry, plIOI);
+        gptShader        = pl_get_api_latest(ptApiRegistry, plShaderI);
+        gptMemory        = pl_get_api_latest(ptApiRegistry, plMemoryI);
+        gptNetwork       = pl_get_api_latest(ptApiRegistry, plNetworkI);
+        gptString        = pl_get_api_latest(ptApiRegistry, plStringInternI);
+        gptProfile       = pl_get_api_latest(ptApiRegistry, plProfileI);
+        gptFile          = pl_get_api_latest(ptApiRegistry, plFileI);
+        gptConsole       = pl_get_api_latest(ptApiRegistry, plConsoleI);
+        gptScreenLog     = pl_get_api_latest(ptApiRegistry, plScreenLogI);
+        gptConfig        = pl_get_api_latest(ptApiRegistry, plConfigI);
+        gptStarter       = pl_get_api_latest(ptApiRegistry, plStarterI);
+        gptDearImGui     = pl_get_api_latest(ptApiRegistry, plDearImGuiI);
+        gptDateTime      = pl_get_api_latest(ptApiRegistry, plDateTimeI);
+        gptVfs           = pl_get_api_latest(ptApiRegistry, plVfsI);
+        gptPak           = pl_get_api_latest(ptApiRegistry, plPakI);
+        gptDateTime      = pl_get_api_latest(ptApiRegistry, plDateTimeI);
+        gptCompress      = pl_get_api_latest(ptApiRegistry, plCompressI);
+
+        gptScreenLog->add_message_ex(0, 15.0, PL_COLOR_32_MAGENTA, 1.5f, "%s", "App Hot Reloaded");
 
         ImPlot::SetCurrentContext((ImPlotContext*)ptDataRegistry->get_data("implot"));
 
@@ -150,29 +174,53 @@ pl_app_load(plApiRegistryI* ptApiRegistry, plAppData* ptAppData)
     const plExtensionRegistryI* ptExtensionRegistry = pl_get_api_latest(ptApiRegistry, plExtensionRegistryI);
 
     // load extensions
-    // load extensions
-    //   * first argument is the shared library name WITHOUT the extension
-    //   * second & third argument is the load/unload functions names (use NULL for the default of "pl_load_ext" &
-    //     "pl_unload_ext")
-    //   * fourth argument indicates if the extension is reloadable (should we check for changes and reload if changed)
     ptExtensionRegistry->load("pl_unity_ext", NULL, NULL, true);
-    ptExtensionRegistry->load("pl_platform_ext", NULL, NULL, false); // provides the file API used by the drawing ext
-    ptExtensionRegistry->load("pl_dear_imgui_ext", NULL, NULL, false); // provides the imgui backend stuff
+    ptExtensionRegistry->load("pl_platform_ext", NULL, NULL, false);
+    ptExtensionRegistry->load("pl_dear_imgui_ext", NULL, NULL, false);
     
     // load required apis
-    gptIO          = pl_get_api_latest(ptApiRegistry, plIOI);
-    gptWindows     = pl_get_api_latest(ptApiRegistry, plWindowI);
-    gptDrawBackend = pl_get_api_latest(ptApiRegistry, plDrawBackendI);
-    gptUi          = pl_get_api_latest(ptApiRegistry, plUiI);
-    gptStarter     = pl_get_api_latest(ptApiRegistry, plStarterI);
-    gptDearImGui   = pl_get_api_latest(ptApiRegistry, plDearImGuiI);
-    gptGfx         = pl_get_api_latest(ptApiRegistry, plGraphicsI);
+    gptWindows       = pl_get_api_latest(ptApiRegistry, plWindowI);
+    gptStats         = pl_get_api_latest(ptApiRegistry, plStatsI);
+    gptGfx           = pl_get_api_latest(ptApiRegistry, plGraphicsI);
+    gptTools         = pl_get_api_latest(ptApiRegistry, plToolsI);
+    gptJobs          = pl_get_api_latest(ptApiRegistry, plJobI);
+    gptDraw          = pl_get_api_latest(ptApiRegistry, plDrawI);
+    gptDrawBackend   = pl_get_api_latest(ptApiRegistry, plDrawBackendI);
+    gptUI            = pl_get_api_latest(ptApiRegistry, plUiI);
+    gptIO            = pl_get_api_latest(ptApiRegistry, plIOI);
+    gptShader        = pl_get_api_latest(ptApiRegistry, plShaderI);
+    gptMemory        = pl_get_api_latest(ptApiRegistry, plMemoryI);
+    gptNetwork       = pl_get_api_latest(ptApiRegistry, plNetworkI);
+    gptString        = pl_get_api_latest(ptApiRegistry, plStringInternI);
+    gptProfile       = pl_get_api_latest(ptApiRegistry, plProfileI);
+    gptFile          = pl_get_api_latest(ptApiRegistry, plFileI);
+    gptConsole       = pl_get_api_latest(ptApiRegistry, plConsoleI);
+    gptScreenLog     = pl_get_api_latest(ptApiRegistry, plScreenLogI);
+    gptConfig        = pl_get_api_latest(ptApiRegistry, plConfigI);
+    gptStarter       = pl_get_api_latest(ptApiRegistry, plStarterI);
+    gptDearImGui     = pl_get_api_latest(ptApiRegistry, plDearImGuiI);
+    gptDateTime      = pl_get_api_latest(ptApiRegistry, plDateTimeI);
+    gptVfs           = pl_get_api_latest(ptApiRegistry, plVfsI);
+    gptPak           = pl_get_api_latest(ptApiRegistry, plPakI);
+    gptDateTime      = pl_get_api_latest(ptApiRegistry, plDateTimeI);
+    gptCompress      = pl_get_api_latest(ptApiRegistry, plCompressI);
+
+    if(gptIO->get_io()->bHotReloadActive)
+    {
+        gptVfs->mount_directory("/shaders", "../shaders", PL_VFS_MOUNT_FLAGS_NONE);
+        gptVfs->mount_directory("/shader-temp", "../shader-temp", PL_VFS_MOUNT_FLAGS_NONE);
+        gptFile->create_directory("../shader-temp");
+    }
+    else
+    {
+        gptVfs->mount_pak("/shader-temp", "shaders.pak", PL_VFS_MOUNT_FLAGS_NONE);
+    }
 
     // use window API to create a window
 
     plWindowDesc tWindowDesc = {
         PL_WINDOW_FLAG_NONE,
-        "Example Basic 5",
+        "Tool Template",
         500,
         500,
         200,
@@ -181,11 +229,20 @@ pl_app_load(plApiRegistryI* ptApiRegistry, plAppData* ptAppData)
     gptWindows->create(tWindowDesc, &ptAppData->ptWindow);
     gptWindows->show(ptAppData->ptWindow);
 
+    // setup starter extension
     plStarterInit tStarterInit = {};
     tStarterInit.tFlags   = PL_STARTER_FLAGS_ALL_EXTENSIONS;
     tStarterInit.ptWindow = ptAppData->ptWindow;
-
+    tStarterInit.tFlags &= ~PL_STARTER_FLAGS_SHADER_EXT;
     gptStarter->initialize(tStarterInit);
+
+    // initialize shader compiler
+    static plShaderOptions tDefaultShaderOptions = {};
+    tDefaultShaderOptions.apcIncludeDirectories[0] = "/shaders/";
+    tDefaultShaderOptions.apcDirectories[0] = "/shaders/";
+    tDefaultShaderOptions.pcCacheOutputDirectory = "/shader-temp/";
+    tDefaultShaderOptions.tFlags = PL_SHADER_FLAGS_AUTO_OUTPUT | PL_SHADER_FLAGS_INCLUDE_DEBUG;
+    gptShader->initialize(&tDefaultShaderOptions);
 
     // wraps up (i.e. builds font atlas)
     gptStarter->finalize();
@@ -207,10 +264,7 @@ pl_app_shutdown(plAppData* ptAppData)
 {
     plDevice* ptDevice = gptStarter->get_device();
     gptGfx->flush_device(ptDevice);
-
-    // cleanup backend resources
     gptDearImGui->cleanup();
-
     gptStarter->cleanup();
     gptWindows->destroy(ptAppData->ptWindow);
     free(ptAppData);
@@ -240,31 +294,28 @@ pl_app_update(plAppData* ptAppData)
     gptDearImGui->new_frame(gptStarter->get_device(), gptStarter->get_render_pass());
 
     ImGui::DockSpaceOverViewport();
-
-    // create a window
-    if(ImGui::Begin("Pilot Light"))
+    if(ImGui::BeginMainMenuBar())
     {
-        if(ImGui::Button("Print To Console"))
+        if(ImGui::BeginMenu("File", false)) ImGui::EndMenu();
+        if(ImGui::BeginMenu("Edit", false)) ImGui::EndMenu();
+        if(ImGui::BeginMenu("Tools", true))
         {
-            printf("Button Clicked");
+            ImGui::MenuItem("Dear ImGui Demo", nullptr, &ptAppData->bShowImGuiDemo);
+            ImGui::MenuItem("ImPlot Demo", nullptr, &ptAppData->bShowImPlotDemo);
+            ImGui::EndMenu();
         }
+        if(ImGui::BeginMenu("Help", false)) ImGui::EndMenu();
+        ImGui::EndMainMenuBar();
     }
-    ImGui::End();
 
-    // just some provided demo windows
-    ImPlot::ShowDemoWindow(nullptr);
-    ImGui::ShowDemoWindow(nullptr);
-
+    if(ptAppData->bShowImPlotDemo)
+        ImPlot::ShowDemoWindow(&ptAppData->bShowImPlotDemo);
+    if(ptAppData->bShowImGuiDemo)
+        ImGui::ShowDemoWindow(&ptAppData->bShowImGuiDemo);
 
     // start main pass & return the encoder being used
     plRenderEncoder* ptEncoder = gptStarter->begin_main_pass();
-
-    // submit Dear ImGui stuff
     gptDearImGui->render(ptEncoder, gptGfx->get_encoder_command_buffer(ptEncoder));
-
-    // allows the starter extension to handle some things then ends the main pass
     gptStarter->end_main_pass();
-
-    // must be the last function called when using the starter extension
     gptStarter->end_frame(); 
 }
