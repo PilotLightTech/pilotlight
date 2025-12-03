33adbf49acf3814ce35c201fa7743be9c99ac467
Jonathan Hoffstadt <jonathan.h.hoffstadt@nasa.gov>
2025-12-03 10:39:00 -0600

    fix: add pl_build support for MSVC 2026 version

--- build/deploy_setup.py
+++ build/deploy_setup.py
@@ -2,7 +2,7 @@ import setuptools
 from codecs import open
 import os
 
-wip_version = "1.2.1"
+wip_version = "1.2.2"
 
 def version_number():
     """This function reads the version number which is populated by github actions"""
