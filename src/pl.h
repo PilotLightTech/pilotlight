08a89e71bd4639a8c5e6214bf9876de8c0bd2492
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2026-03-06 11:29:13 -0600

    chore: tag version 0.8.0

--- src/pl.h
+++ src/pl.h
@@ -24,7 +24,7 @@ Index of this file:
 #define PL_H
 
 // global version XYYZZ
-#define PILOT_LIGHT_VERSION_STRING "0.8.0 WIP"
+#define PILOT_LIGHT_VERSION_STRING "0.8.0"
 #define PILOT_LIGHT_VERSION_NUM     00800
 #define PILOT_LIGHT_VERSION         {0, 8, 0}
 
